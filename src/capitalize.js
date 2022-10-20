ee65256a9c00c99e5f0e821ca6f470c14c53a79c
sergey028 <sshurdakov@gmail.com>
2022-10-20 22:18:34 +0300

    fix node_modules and file capitalieze.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -3,10 +3,10 @@ const capitalize = (text) => {
       return "";
     }
   
-    const firstChar = text[0].toUpperCase()
-    const restSubstring = text.slice(1)
+    const firstChar = text[0].toUpperCase();
+    const restSubstring = text.slice(1);
   
-    return `${firstChar}${restSubstring}`
+    return `${firstChar}${restSubstring}`;
   }
 
   export default capitalize;
