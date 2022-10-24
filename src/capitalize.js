4a0ad8e1c01b6c63cafa4a67fb592b5da47cea3b
sergey028 <sshurdakov@gmail.com>
2022-10-24 22:44:00 +0300

    fix capitalize.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,12 +1,11 @@
-const capitalize = (text) => {
+
+export function capitalize(text) {
     if (text === "") {
       return "";
     }
-  
+
     const firstChar = text[0].toUpperCase();
     const restSubstring = text.slice(1);
   
     return `${firstChar}${restSubstring}`;
   }
-
-  export default capitalize;
